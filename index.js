dc94935c30649d8c73806305955df5b71b096e68
Bennie_dev <128320706+Bennie84@users.noreply.github.com>
2026-01-04 18:54:22 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const toggleBtn = document.getElementById('theme-toggle');
+const icon = toggleBtn.querySelector('i');
+toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    if(document.body.classList.contains('dark')){
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');    
+    } else {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    }
+    
+    });
+
+
+
+
